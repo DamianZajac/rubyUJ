4bdb778cef51dd17c03a69b7447a30d1764d5e06
DamianZajac <damian.zajc@gmail.com>
2015-03-23 22:54:16 +0000

    zajecia3-brakbledu

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+ 
   def new
     @article = Article.new
   end
@@ -33,8 +33,8 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
-end
-  
+  end
+ 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
@@ -42,8 +42,8 @@ end
     redirect_to articles_path
   end
  
-private
-  def article_params
-    params.require(:article).permit(:title, :text)
-  end
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317143635) do
+ActiveRecord::Schema.define(version: 20150323224139) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
