d67c81559a60533cb9b2098566cdad0d9a7efc48
DamianZajac <damian.zajc@gmail.com>
2015-04-28 13:32:14 +0000

    Add_delete_picture

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,17 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  attr_accessor :remove_picture
   validates :title, presence: true,
                     length: { minimum: 5 }
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   validates_attachment_file_name :picture, :matches => [/gif\Z/, /png\Z/, /jpe?g\Z/]
   validates_with AttachmentSizeValidator, :attributes => :picture, :less_than => 1.megabytes
+  
+  before_save :delete_picture, if: ->{ remove_picture == '1' && !picture_updated_at_changed? }
+  
+  private
+    def delete_picture
+      self.picture = nil
+    end
 end
